566a01bed53817310810de88fa3829558d2363e1
Cole Faust <colecfaust@gmail.com>
2013-12-19 15:52:32 -0800

    Initial Commit

---
+++ MMO/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MMO
+//
+//  Created by Cole Faust on 12/19/13.
+//  Copyright (c) 2013 Cole Faust. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ MMO/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MMO
+//
+//  Created by Cole Faust on 12/19/13.
+//  Copyright (c) 2013 Cole Faust. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
